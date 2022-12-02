807d0a0f5d593ee371af6e437206905a904434f0
rezkovmv <rezkovmv@63pokupki.ru>
2022-12-02 14:53:31 +0400

    created service for working with tree items

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { ItemI } from "./interface/TestWorkI";
+import { TreeStore } from "./service/TreeStore";
+
+const items: ItemI[] = [
+    { id: 1, parent: 'root' },
+    { id: 2, parent: 1, type: 'test' },
+    { id: 3, parent: 1, type: 'test' },
+
+    { id: 4, parent: 2, type: 'test' },
+    { id: 5, parent: 2, type: 'test' },
+    { id: 6, parent: 2, type: 'test' },
+
+    { id: 7, parent: 4, type: null },
+    { id: 8, parent: 4, type: null },
+];
+
+const ts = new TreeStore(items);
+
+console.log(ts.getAllParents(7));
+
---
+++ src/service/TreeStore.ts
@@ -0,0 +1,73 @@
+import { TreeStoreI, ItemI } from "../interface/TestWorkI";
+
+export class TreeStore implements TreeStoreI {
+    public items: ItemI[] = [];
+    public treeItemsById: Record<number, ItemI> = {};
+    public treeGroupItemsByParentId: Record<number, ItemI[]> = {};
+
+    constructor(items: ItemI[]) {
+        this.items = items;
+        if (this.items?.length) {
+            for (let i = 0; i < this.items.length; i++) {
+                const item = this.items[i];
+                this.treeItemsById[item.id] = item;
+                
+                if (!this.treeGroupItemsByParentId[item.parent]) {
+                    this.treeGroupItemsByParentId[item.parent] = [];
+                }
+                this.treeGroupItemsByParentId[item.parent].push(item);
+            }
+        }
+    }
+
+    public getAll() {
+        return this.items;
+    }
+
+    public getItem(itemId: number | string) {
+        return this.treeItemsById[itemId];
+    }
+
+    public getChildren(parentId: number | string) {
+        return this.treeGroupItemsByParentId[parentId] ?? [];
+    }
+
+    public getAllChildren(parentId: number | string) {
+        const childrensFirstLevel = this.getChildren(parentId);
+        const stackAllChildrens = [...childrensFirstLevel];
+        const result: ItemI[] = [];
+
+        while (stackAllChildrens.length > 0) {
+            const item = stackAllChildrens.pop();
+            if (item) {
+                result.push(item);
+                const childrensLowLevel = this.getChildren(item.id);
+                stackAllChildrens.push(...childrensLowLevel);
+            }
+        }
+
+        return result;
+    }
+
+    public getAllParents(itemId: number | string) {
+        const item = this.getItem(itemId);
+        const result: ItemI[] = [];
+        if (item) {
+            const parent = this.getItem(item.parent);
+            if (parent) {
+                const stackAllParents = [parent];
+                result.push(parent);
+                while (stackAllParents.length > 0) {
+                    const item = stackAllParents.pop();
+                    const parent = this.getItem(item.parent);
+                    if (parent) {
+                        result.push(parent);
+                        stackAllParents.push(parent);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
